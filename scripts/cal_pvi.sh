3041c99bd11ac5bcd25438761119456778df296b
Akiyasu <137394841+DMK041218@users.noreply.github.com>
2026-02-10 15:46:30 +0800

    Update paths in cal_pvi.sh for local execution

--- scripts/cal_pvi.sh
+++ scripts/cal_pvi.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# 1. 切到 LLaVA 根目录
-cd /gemini/code/dmk/LLaVA
+cd LLaVA
 
-# 2. 设置环境变量
+
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 export CUDA_VISIBLE_DEVICES=3
 
 python scripts/v1_5/cal_pvi.py \
-    --model_name_or_path /gemini/code/dmk/model/llava-v1.5-7b_vicuna \
-    --data_path /gemini/code/dmk/LLaVA/datasets/download/llava-v1.5-instruct/llava_v1_5_mix665k.json \
-    --image_folder /gemini/code/dmk/LLaVA/datasets/download/llava-v1.5-instruct \
-    --pretrain_mm_mlp_adapter /gemini/code/dmk/model/mm_projector.bin \
+    --model_name_or_path ./llava-v1.5-7b_vicuna \
+    --data_path ./llava_v1_5_mix665k.json \
+    --image_folder ./llava-v1.5-instruct \
+    --pretrain_mm_mlp_adapter ./mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -19,7 +18,7 @@ python scripts/v1_5/cal_pvi.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir /gemini/code/dmk/LLaVA/results \
+    --output_dir ./LLaVA/results \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 1 \
